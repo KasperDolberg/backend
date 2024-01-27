9315dcc6efa11eef764f3c4712f6336ed73c2f24
Lautaro Ayala <nicolelautarole@gmal.com>
2024-01-27 10:05:15 -0300

    Re-entrega solucionado todo funcionado correctamente

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -1,6 +1,98 @@
 import fs from "fs/promises"
 
-// Reemplazamos la siguiente clase ProductManager por la que realizaste la entrega pasada
-class ProductManager {} 
+class ProductManager {
+	constructor(path) {
+		this.path = path;
+	}
+
+	async addProduct({ title, description, price, thumbnail, code, stock }) {
+		if (!title || !description || !price || !thumbnail || !code || !stock) {
+			console.log("¡Faltan propiedades del producto!");
+			return null;
+		}
+
+		const products = await this.getProducts();
+
+		const foundCode = products.find((prod) => prod.code === code);
+		if (foundCode) {
+			console.log("Codigo de producto repetido");
+			return null;
+		}
+
+		const id = products.length ? products[products.length - 1].id + 1 : 1;
+
+		const newProduct = {
+			title,
+			description,
+			price,
+			thumbnail,
+			code,
+			stock,
+			id,
+		};
+
+		products.push(newProduct);
+		await this.writeFile(products);
+		return id;
+	}
+
+	async getProducts() {
+		try {
+			const json = await fs.readFile(this.path, "utf-8");
+			const products = JSON.parse(json);
+			return products;
+		} catch (error) {
+			await fs.writeFile(this.path, "[]");
+			return [];
+		}
+	}
+
+	async getProductById(id) {
+		const products = await this.getProducts();
+		const foundProduct = products.find((prod) => prod.id === id);
+		if (!foundProduct) {
+			console.log("¡Not found!");
+			return null;
+		}
+		return foundProduct;
+	}
+
+	async updateProduct(id, obj) {
+		const products = await this.getProducts();
+		const indexProduct = products.findIndex((product) => product.id === id);
+
+		if (indexProduct < 0) {
+			console.log("¡Not found!");
+			return null;
+		}
+
+		products[indexProduct] = { ...products[indexProduct], ...obj };
+
+		await this.writeFile(products);
+		return products[indexProduct];
+	}
+
+	async deleteProduct(id) {
+		let products = await this.getProducts();
+		const orginalLength = products.length;
+		products = products.filter((product) => product.id !== id);
+		if (orginalLength === products.length) {
+			console.log("¡Not found!");
+			return null;
+		}
+
+		await this.writeFile(products);
+		return true;
+	}
+
+	async writeFile(products) {
+		try {
+			const json = JSON.stringify(products, null, 2);
+			await fs.writeFile(this.path, json);
+		} catch (error) {
+			console.log("Error al escribir el archivo: ", error);
+		}
+	}
+}
 
 export default ProductManager
--- src/app.js
+++ src/app.js
@@ -7,11 +7,23 @@ const manager = new ProductManager("productos.json");
 const app = express();
 
 app.get("/products", async (req, res) => {
-	// --- Escribe aqui tu codigo ---
+	const limit = parseInt(req.query.limit);
+	const allProduct = await manager.getProducts();
+	if(limit && !isNaN(limit)) {
+		return res.json({ status: 200, payload: allProduct.slice(0, limit), limit})
+	}
+	res.json({status:200, payload: allProduct, limit: null})
 });
 
 app.get("/products/:id", async (req, res) => {
-	// --- Escribe aqui tu codigo ---
+	const idProduct = req.params.id;
+	if (isNaN(idProduct)) {
+        return res.status(400).json({ error: "Invalid ID format, must be a number", status: 404 });
+    }
+	const product = await manager.getProductById(Number(idProduct));
+	if(!product) return res.status(404) .json({error: "Product not found", status: 404})
+	res.json({ payload: product, status: 200 })
 });
+
 
 app.listen(8080, () => console.log("¡Servidor arriba escuchando en el puerto 8080!"));
